49fa03436ff2b49bc3d888dc0d3d9a7930e78c3d
estelikm973 <estelikm973@gmail.com>
2024-07-05 03:29:39 +0600

    test: event button

--- app/table-view/page.tsx
+++ app/table-view/page.tsx
@@ -1,8 +1,11 @@
+import { EventButton } from "@/components/Analytics/EventButton";
+
 export default function TableView() {
   return (
     <div className="max-w-screen-2xl mx-auto py-16">
       <main>
         <h1 className="text-3xl font-medium">Table View</h1>
+        <EventButton />
       </main>
     </div>
   );
---
+++ components/Analytics/EventButton.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { sendGAEvent } from "@next/third-parties/google";
+
+export function EventButton() {
+  return (
+    <div>
+      <button
+        onClick={() => sendGAEvent({ event: "buttonClicked", value: "xyz" })}
+      >
+        Send Event
+      </button>
+    </div>
+  );
+}
