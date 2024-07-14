102c05a9ebe51e89c0b085f4a9500ddc5cf0d974
estelikm973 <estelikm973@gmail.com>
2024-07-14 11:21:07 +0600

    removed unnecessary code

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
-import { getGAMetricSum } from "@/actions/analytics";
-import { getGridData } from "@/actions/grid";
-import { getTableData } from "@/actions/table";
-
-export default async function Home() {
-  const data = await getGridData();
-
+export default function Home() {
   return (
     <div className="max-w-screen-xl mx-auto">
       <main className="my-8">
         <h1 className="text-3xl font-medium mb-8">Home</h1>
-        <div>{JSON.stringify(data, null, 4)}</div>
       </main>
     </div>
   );
